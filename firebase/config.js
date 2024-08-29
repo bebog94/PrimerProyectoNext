5380ccda235ee7226f101d1f8a92df3d84451f3c
bebog <bebo.garavano@gmail.com>
2024-08-28 23:23:59 -0300

    first commit

---
+++ firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8eeIqLENKFwpnKeU4B7m3NL63FlVE1gw",
+  authDomain: "coder-next-7fc45.firebaseapp.com",
+  projectId: "coder-next-7fc45",
+  storageBucket: "coder-next-7fc45.appspot.com",
+  messagingSenderId: "156318016334",
+  appId: "1:156318016334:web:0372f4306430a5acc1741f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
